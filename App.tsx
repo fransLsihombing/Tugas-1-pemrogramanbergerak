5f8bcbde11e77de37d2d0cb7842a7e73fc5ab55c
Fransiskus Parade L Sihombing <fransiskussihombing04@gmail.com>
2024-07-02 14:55:41 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,92 +1,48 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, Text, ToastAndroid, Image, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {View, Text, Image, StyleSheet, FlatList} from 'react-native';
 
-function LoginForm({ onLogin }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: 'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1590455428/go4c2dvipmmt0b93gdxi.png' }} // Ganti URL dengan URL gambar Anda
-        style={styles.image}
-      />
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        style={styles.input}
-      />
-      <Button title="Login" onPress={() => onLogin(username, password)} />
-    </View>
-  );
-}
-
-function HomeScreen() {
-  return <Text>Beranda</Text>;
-}
-
-function SettingsScreen() {
-  return <Text>Pengaturan Android</Text>;
-}
-
-const Tab = createBottomTabNavigator();
-
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = (username, password) => {
-    if (username === '' || password === '') {
-      ToastAndroid.show('Username dan Password harus diisi!', ToastAndroid.SHORT);
-      return; // Menghentikan fungsi jika username atau password kosong
-    }
-    // Proses login jika validasi terpenuhi
-    setIsLoggedIn(true);
-    ToastAndroid.show('Anda berhasil login!', ToastAndroid.SHORT);
-  };
-
-  if (!isLoggedIn) {
-    return <LoginForm onLogin={handleLogin} />;
-  }
+const App = () => {
+  const products = [
+    {id: 1, name: 'Sepatu Sneakers Pria Hitam', price: 'Rp 250.000'},
+    {id: 2, name: 'Sepatu Sneakers Pria Hitam', price: 'Rp 250.000'},
+    {id: 3, name: 'Sepatu Sneakers Pria Hitam', price: 'Rp 250.000'},
+    {id: 4, name: 'Sepatu Sneakers Pria Hitam', price: 'Rp 250.000'},
+    {id: 5, name: 'Sepatu Sneakers Pria Hitam', price: 'Rp 250.000'},
+    {id: 6, name: 'Sepatu Sneakers Pria Hitam', price: 'Rp 280.000'},
+    {id: 7, name: 'Sepatu Sneakers Pria Hitam', price: 'Rp 280.000'},
+  ];
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <FlatList
+      data={products}
+      renderItem={({item}) => (
+        <View style={styles.itemContainer}>
+          <Image
+            source={{
+              uri: 'https://images.tokopedia.net/img/cache/700/VqbcmM/2022/1/16/1abf9771-e4c9-47c8-b77a-95a05fc4bb34.jpg',
+            }}
+            style={styles.image}
+          />
+          <Text>{item.name}</Text>
+          <Text>{item.price}</Text>
+        </View>
+      )}
+      keyExtractor={item => item.id.toString()}
+      numColumns={3}
+    />
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  itemContainer: {
     flex: 1,
+    margin: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 16,
   },
   image: {
     width: 100,
     height: 100,
-    marginBottom: 16,
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 12,
-    paddingLeft: 8,
   },
 });
 
